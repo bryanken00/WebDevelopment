6a67ea5a6f7a0c923aad9cd50f8884790e13e5a4
bryanken00 <bryanken01230@gmail.com>
2023-05-07 17:40:47 +0800

    Cleaning code

--- thesis1_website/loginpage/index.php
+++ thesis1_website/loginpage/index.php
@@ -16,7 +16,9 @@
 
 <body>
 
-    <?php include('topNav/topNav.php')?>
+    <div class="log">
+        <?php include('../includesPHP/topNav.php')?>
+    </div>
 
     <div class="logInCon">
 
@@ -65,56 +67,7 @@
 
     
 
-    <div class="footer">
-
-        <div class="col1">
-
-            <img src="../image\logo/KBN_Logo.png" class="logof">
-
-            <div class="location">
-                <a class="locIcn"><i class="fa-sharp fa-solid fa-location-dot"></i></a>
-                <p class="loc">Binangonan, Philippines, 1940</p>
-            </div>
-
-            <div class="telephone">
-                <a class="telIcn"><i class="fa-solid fa-phone"></i></a>
-                <p class="tel">286961198</p>
-            </div>
-
-            <div class="email">
-                <a class="emailIcn"><i class="fa-solid fa-envelope"></i></a>
-                <p class="mail">kbnskinessentialsltdco@gmail.com</p>
-            </div>
-
-        </div>
-
-        <div class="col2">
-
-            <p class="fTitle">Offers</p>
-
-            <p class="fContent">Manufacture</p>
-
-        </div>
-
-        <div class="col3">
-
-            <p class="fTitle">Information</p>
-
-        </div>
-
-        <div class="col4">
-
-            <p class="fTitle">Privacy Policy</p>
-
-            <p class="fContent">Website Privacy Policy</p>
-            <p class="fContent">Request for Return</p>
-            <p class="fContent">Legibility and Validity</p>
-
-        </div>
-
-    </div>
-
-    <p class="cp">Copyright 2023 All Rights Reserved.</p>
+    <?php include('../includesPHP/footer.php')?>
 
     <script src="../javascript/web.js"></script>
 
