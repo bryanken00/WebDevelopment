76aa79fb049981edf859e34dc8b97a2315d27c6f
bryanken00 <bryanken01230@gmail.com>
2023-05-10 00:16:23 +0800

    Add to Cart - Img Path FIX

--- thesis1_website/includesPHP/AddtoCart.php
+++ thesis1_website/includesPHP/AddtoCart.php
@@ -1,12 +1,17 @@
 <?php
 
 // print_r($_POST);
+
 // Retrieve the data sent by the AJAX request
 $productImg = $_POST['productImg'];
 $productLabel = $_POST['productLabel'];
 $productWeight = $_POST['productWeight'];
 $productPrice = $_POST['productPrice'];
+$dirName = $_POST['dirName'];
 
+// echo $dirName;
+
+$imgPath = $dirName . "/" . $productImg;
 
 include('../includesPHP/database.php');
 
@@ -15,9 +20,9 @@ $result = $conn->query("SELECT * FROM tblOrders WHERE UserID = '1'");
 $row = $result->fetch_assoc();
 $data = $row['OrderList'];
 if($data == null){
-    $sql = "UPDATE tblorders SET OrderList = CONCAT(OrderList,'$productImg+$productLabel+$productWeight+$productPrice') WHERE UserID = 1";
+    $sql = "UPDATE tblorders SET OrderList = CONCAT(OrderList,'$imgPath+$productLabel+$productWeight+$productPrice') WHERE UserID = 1";
 }else{
-    $sql = "UPDATE tblorders SET OrderList = CONCAT(OrderList,',$productImg+$productLabel+$productWeight+$productPrice') WHERE UserID = 1";
+    $sql = "UPDATE tblorders SET OrderList = CONCAT(OrderList,',$imgPath+$productLabel+$productWeight+$productPrice') WHERE UserID = 1";
 }
 
 echo $sql;
