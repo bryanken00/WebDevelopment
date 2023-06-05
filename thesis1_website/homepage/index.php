0daf01b230b05998fe7f1891550fa2fb4d468fb0
ravenrosee <ravenberenguila@gmail.com>
2023-06-05 22:39:02 +0800

    .

--- thesis1_website/homepage/index.php
+++ thesis1_website/homepage/index.php
@@ -51,9 +51,7 @@
 
     </div>
 
-    <a class="messenger">
-        <i class="fa-brands fa-facebook-messenger"></i>
-    </a>
+    
 
     <br><br>
 
@@ -106,37 +104,6 @@
         </div>
 
     </div>
-    
-    <!-- Messenger Chat Plugin Code -->
-    <div id="fb-root"></div>
-
-    <!-- Your Chat Plugin code -->
-    <div id="fb-customer-chat" class="fb-customerchat">
-    </div>
-
-    <script>
-      var chatbox = document.getElementById('fb-customer-chat');
-      chatbox.setAttribute("page_id", "112990251816490");
-      chatbox.setAttribute("attribution", "biz_inbox");
-    </script>
-
-    <!-- Your SDK code -->
-    <script>
-      window.fbAsyncInit = function() {
-        FB.init({
-          xfbml            : true,
-          version          : 'v17.0'
-        });
-      };
-
-      (function(d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) return;
-        js = d.createElement(s); js.id = id;
-        js.src = 'https://connect.facebook.net/en_US/sdk/xfbml.customerchat.js';
-        fjs.parentNode.insertBefore(js, fjs);
-      }(document, 'script', 'facebook-jssdk'));
-    </script>
 
     <?php include('../includesPHP/footer.php')?>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
