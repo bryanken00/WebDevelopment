cbe9504b8e7fafe9fdd90f8b48a9b25a827fc10e
ravenrosee <ravenberenguila@gmail.com>
2023-07-07 19:33:27 +0800

    design

--- thesis1_website/application/index.php
+++ thesis1_website/application/index.php
@@ -19,62 +19,71 @@
         <?php include('../includesPHP/topNav.php')?>
     </div>
 
-    <img class="fphoto" src="resources/KBN Products.jpg">
+    <div class="registrationForm">
 
-    <form method="post" class="cusInfo">
-
-        <p class="applicationTitle">Registration</p>
-
-        <div class="grind1">
-            <label class="applicationlbl">Name:</label>
-            <input class="regFormLastName" placeholder="Last Name" name="regFormLastName" required>
-            <input class="regFormFirstName" placeholder="First Name" name="regFormFirstName" required>
-            <input class="regFormMI" placeholder="MI" name="regFormMI">
-
-        </div>
-
-        <div class="grind1">
-            <label class="applicationlbl">Contacts:</label>
-            <input class="regFormContact" placeholder="Contact No." name="regFormContact" type='number' required>
-            <input class="regFormEmail" placeholder="Email" name="regFormEmail" required>
-
-        </div>
-
-        <div class="grind1">
-            <label class="applicationlbl">Address:</label>
+        <div class="innerRegistrationForm">
             
-            <div class="add">
-                <select class='regFormRegion' id="regionSelect" required>
-                    <option value="Default">Region</option>
-                </select>
+            <img class="fphoto" src="resources/KBN Products.jpg">
 
-                <select class='regFormProvince' id="provinceSelect" required>
-                    <option value="Default">Province</option>
-                </select>
+            <form method="post" class="cusInfo">
 
-                <select class='regFormCity' id="citySelect" required>
-                    <option value="Default">City</option>
-                </select>
+                <p class="applicationTitle">Registration</p>
 
-                <select class='regFormBarangay' id="barangaySelect" required>
-                    <option value="Default">Barangay</option>
-                </select>
-            </div>
-                <input class="address2" placeholder="Street Name, Building, House No." required>
-                <input class="zipc" placeholder="Zip Code" type="Number" required>
+                <div class="grind1">
+                    <label class="applicationlbl">Name:</label>
+                    <input class="regFormLastName" placeholder="Last Name" name="regFormLastName" required>
+                    <input class="regFormFirstName" placeholder="First Name" name="regFormFirstName" required>
+                    <input class="regFormMI" placeholder="MI" name="regFormMI">
+
+                </div>
+
+                <div class="grind1">
+                    <label class="applicationlbl">Contacts:</label>
+                    <input class="regFormContact" placeholder="Contact No." name="regFormContact" type='number' required>
+                    <input class="regFormEmail" placeholder="Email" name="regFormEmail" required>
+
+                </div>
+
+                <div class="grind1">
+                    <label class="applicationlbl">Address:</label>
+                    
+                    <div class="add">
+                        <select class='regFormRegion' id="regionSelect" required>
+                            <option value="Default">Region</option>
+                        </select>
+
+                        <select class='regFormProvince' id="provinceSelect" required>
+                            <option value="Default">Province</option>
+                        </select>
+
+                        <select class='regFormCity' id="citySelect" required>
+                            <option value="Default">City</option>
+                        </select>
+
+                        <select class='regFormBarangay' id="barangaySelect" required>
+                            <option value="Default">Barangay</option>
+                        </select>
+                    </div>
+                        <input class="address2" placeholder="Street Name, Building, House No." required>
+                        <input class="zipc" placeholder="Zip Code" type="Number" required>
+                </div>
+
+                <div class="grind1">
+                    <label class="applicationlbl">Product Description</label>
+                    <input class="prodes" placeholder="Product Description" required>
+                </div>
+
+                <br>
+                
+                <button class="appbtn" onclick="passingData()">Submit</button>
+                    
+            </form>
+
         </div>
 
-        <div class="grind1">
-            <label class="applicationlbl">Product Description</label>
-            <input class="prodes" placeholder="Product Description" required>
-        </div>
+    </div>
 
-        <br>
-        
-        <button class="appbtn" onclick="passingData()">Submit</button>
-            
-    </form>
-
+    
     <br><br>
 
     <?php include('../includesPHP/footer.php')?>
