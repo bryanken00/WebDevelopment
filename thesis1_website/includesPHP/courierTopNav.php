c7a2b6712292ac7dbca4211f122f2427d4356b72
ravenrosee <ravenberenguila@gmail.com>
2023-08-17 09:44:38 +0800

    .

--- thesis1_website/includesPHP/courierTopNav.php
+++ thesis1_website/includesPHP/courierTopNav.php
@@ -4,17 +4,16 @@ if(session_status() == PHP_SESSION_NONE)
 ?>
 
 <div class="courierSide-navbar">
-        
-        <div class="courierSide-user">
 
         <input type="checkbox" id="check">
+        
+        <div class="courierSide-user">
 
         <label for="check">
 
             <a class="courierSide-icn" onclick="courierUserFunction()">
                 <i class="fa-solid fa-user" id="nav-slide"></i>
                 <a><?php echo $_SESSION['Username']?></a>
-                <i class="fa-solid fa-xmark" id="nav-close"></i>
             </a>
 
         </label>
