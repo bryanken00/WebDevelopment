4967c23cb98085ae7944b530ff301c3b7c3d7ba1
raven berenguila <you@example.com>
2023-09-13 21:46:46 +0800

    .

--- thesis1_website/about/index.php
+++ thesis1_website/about/index.php
@@ -21,122 +21,102 @@
 
         <div class="innerAbtContent">
 
-            <div class="imgM">
-            
+        <p class="abt-title">10 Reasons Why We Are Your Manuf!</p>
+
+            <div class="abt-grid">
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line">  
+                    <p class="abt-item-title">1. EASE OF DOING BUSINESS</p>
+
+                    <p class="abt-item-info">>Kapag madaling kausap ang manufacturer, smooth ang flow ng business transactions at madali magpatakbo ng negosyo. We pride ourselves with our warm and accommodating client relationship staffs!</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line">
+                    <p class="abt-item-title">2. FDA-LICENSEDS</p>
+                    
+                    <p class="abt-item-info">Tayo sa KBN Skin Essentials, lisensyado para sigurado! To verify, check the official website of FDA: <a href="https://verification.fda.gov.ph/Cosmetic_Manufacturerlist.php?fbclid=IwAR1l7m5l1Zusi_-7QvEztsoWrphpE7d-Q_JFnXhMqcDQrshYCI7eFPUCpYo">FDA-LICENSED</a> and Look for KBN Skin Essentials (FDA LTO License 3000004523452).</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line">
+                    <p class="abt-item-title">3. INDUSTRY EXPERT</p>
+                    
+                    <p class="abt-item-info">Sumusunod tayo sa ASEAN Directive for Good Manufacturing Practice at we are fully equipped with machineries, manpower and industry experts to accommodate all your needs!</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line">
+                    <p class="abt-item-title">4. BROAD NUMBER OF FORMULATIONS</p>
+                    
+                    <p class="abt-item-info">Partner natin ang ilang mga international laboratories in USA, Thailand, Korea at iba pa! We have access to huge number of formulations ng mga trending products both locally and internationally. Kaya confident tayo na dalhin ang business mo even in the global level!</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                    <p class="abt-item-title">5. CUSTOMIZED AND CREATED FOR YOU</p>
+                    
+                    <p class="abt-item-info">Have a unique, stand-out product in mind? Define na ba natin ang unique selling points ng beauty line mo? From the ingredients to packaging, siguraduhin natin na you will have the most competitive product in the market.</p>
+
+                </div>
+
             </div>
-    
-            <div class="reasons">
-    
-                <div class="d1">
-    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg" src="resources/abtImg1.jpg">
-                        
-                        <div class="dropdownCon">
-                            <p class="rTi">1. EASE OF DOING BUSINESS</p>
-                            <p class="rTinfo">Kapag madaling kausap ang manufacturer, smooth ang flow ng business transactions at madali magpatakbo ng negosyo. We pride ourselves with our warm and accommodating client relationship staffs!</p>
-                        </div>
-    
-                    </div>
+
+            <div class="abt-grid">
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line"> 
+                    <p class="abt-item-title">6. LOWEST PRICE BUT HIGHEST QUALITY</p>
+
+                    <p class="abt-item-info">Nakakalula ba ang puhunang dapat ilabas mo sa ibang manuf? Sobrang mahal ba ng per product nila? Talk to us and discover kung paano natin naibibigay ang highest quality and product effectiveness sa mas affordable na presyo. Remember, gusto namin, lamang ang mark-up mo!</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line"> 
+                    <p class="abt-item-title">7. LOW MOQ (Minimum Order Quantity)</p>
                     
-                    <div class="dropdown">
-    
-                        <img class="aboutImg2" src="resources/abtImg2.jpg">
-                        
-                        <div class="dropdownCon2">
-                            <p class="rTi">2. FDA-LICENSEDS</p>
-                            <p class="rTinfo">Tayo sa KBN Skin Essentials, lisensyado para sigurado! To verify, check the official website of FDA: <a href="https://verification.fda.gov.ph/Cosmetic_Manufacturerlist.php?fbclid=IwAR1l7m5l1Zusi_-7QvEztsoWrphpE7d-Q_JFnXhMqcDQrshYCI7eFPUCpYo">FDA-LICENSED</a> and Look for KBN Skin Essentials (FDA LTO License 3000004523452).</p>
-                        </div>
-                    </div>
+                    <p class="abt-item-info">Sa ibang manuf, nape-pressure ka ba sa 10k pcs nila? O sa 5k pcs nila? Paano kung ang kaya mo pa lang ay 1k? Pag-usapan natin ‘yan! Tulad mo, hate din namin ang mga istrikto sa MOQ: pro-client tayo!</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line"> 
+                    <p class="abt-item-title">8. SIGNED CONTRACT FOR YOUR PROTECTION</p>
                     
-                    <div class="dropdown">
-    
-                        <img class="aboutImg" src="resources/abtImg3.jpg">
+                    <p class="abt-item-info">To protect your business interest, we will be happy na magbigay ng contract sa’yo, for your ease of mind and so you can also manage your expectations mula sa amin bilang manuf. We value business honesty and integrity above all.</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                <div class="vertical-line"> 
+                    <p class="abt-item-title">9. PROFESSIONAL GUIDANCE FOR STARTERS</p>
+                    
+                    <p class="abt-item-info">Hindi pa alam paano magsisimula? Ano ba ang dapat unahin? Allow us to guide you and show the way! Dito sa KBN, we assign dedicated staff who can tell you everything you need to know to start your own skincare business.</p>
+                </div>
+                </div>
+
+                <div class="abt-grid-item">
+
+                    <p class="abt-item-title">10. CAN CATER HUGE MOQ</p>
+                    
+                    <p class="abt-item-info">Confident ka na sa status ng business mo and kailangan mo ng manuf na may strong and steady output? We can cater your high volume orders! The higher the volume, the higher the discount too, para maximized ang income mo.</p>
+
+                </div>
                 
-                        <div class="dropdownCon">
-                            <p class="rTi">3. INDUSTRY EXPERT</p>
-                            <p class="rTinfo">Sumusunod tayo sa ASEAN Directive for Good Manufacturing Practice at we are fully equipped with machineries, manpower and industry experts to accommodate all your needs!</p>
-                        </div>
-                    </div>
-                    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg2" src="resources/abtImg2.jpg">
-                        
-                        <div class="dropdownCon2">
-                            <p class="rTi">4. BROAD NUMBER OF FORMULATIONS</p>
-                            <p class="rTinfo">Partner natin ang ilang mga international laboratories in USA, Thailand, Korea at iba pa! We have access to huge number of formulations ng mga trending products both locally and internationally. Kaya confident tayo na dalhin ang business mo even in the global level!</p>
-                        </div>
-                    </div>
-                    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg" src="resources/abtImg4.jpg">
-                        
-                        <div class="dropdownCon">
-                            <p class="rTi">5. CUSTOMIZED AND CREATED FOR YOU</p>
-                            <p class="rTinfo">Have a unique, stand-out product in mind? Define na ba natin ang unique selling points ng beauty line mo? From the ingredients to packaging, siguraduhin natin na you will have the most competitive product in the market.</p>
-                        </div>
-                    </div>
-                    
-    
-                </div>
-    
-                <div class="d2">
-    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg2" src="resources/abtImg1.jpg">
-                        
-                        <div class="dropdownCon2">
-                            <p class="rTi">6. LOWEST PRICE BUT HIGHEST QUALITY</p>
-                            <p class="rTinfo">Nakakalula ba ang puhunang dapat ilabas mo sa ibang manuf? Sobrang mahal ba ng per product nila? Talk to us and discover kung paano natin naibibigay ang highest quality and product effectiveness sa mas affordable na presyo. Remember, gusto namin, lamang ang mark-up mo!</p>
-                        </div>
-                    </div>
-                    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg2" src="resources/abtImg1.jpg">
-                        
-                        <div class="dropdownCon">
-                            <p class="rTi">7. LOW MOQ (Minimum Order Quantity)</p>
-                            <p class="rTinfo">Sa ibang manuf, nape-pressure ka ba sa 10k pcs nila? O sa 5k pcs nila? Paano kung ang kaya mo pa lang ay 1k? Pag-usapan natin ‘yan! Tulad mo, hate din namin ang mga istrikto sa MOQ: pro-client tayo!</p>
-                        </div>
-                    </div>
-                    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg2" src="resources/abtImg1.jpg">
-                        
-                        <div class="dropdownCon2">
-                            <p class="rTi">8. SIGNED CONTRACT FOR YOUR PROTECTION</p>
-                            <p class="rTinfo">To protect your business interest, we will be happy na magbigay ng contract sa’yo, for your ease of mind and so you can also manage your expectations mula sa amin bilang manuf. We value business honesty and integrity above all.</p>
-                        </div>
-                    </div>
-                    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg" src="resources/abtImg1.jpg">
-                        
-                        <div class="dropdownCon">
-                            <p class="rTi">9. PROFESSIONAL GUIDANCE FOR STARTERS</p>
-                            <p class="rTinfo">Hindi pa alam paano magsisimula? Ano ba ang dapat unahin? Allow us to guide you and show the way! Dito sa KBN, we assign dedicated staff who can tell you everything you need to know to start your own skincare business.</p>
-                        </div>
-                    </div>
-                    
-                    <div class="dropdown">
-    
-                        <img class="aboutImg2" src="resources/abtImg1.jpg">
-                        
-                        <div class="dropdownCon2">
-                            <p class="rTi">10. CAN CATER HUGE MOQ</p>
-                            <p class="rTinfo">Confident ka na sa status ng business mo and kailangan mo ng manuf na may strong and steady output? We can cater your high volume orders! The higher the volume, the higher the discount too, para maximized ang income mo.</p>
-                        </div>
-                    </div>
-                    
-                </div>
-      
             </div>
 
         </div>
