215f0a8b14146fe095fdc5b2a44980262a106f86
raven berenguila <you@example.com>
2023-09-14 22:21:46 +0800

    warehouse

---
+++ thesis1_website/warehouse/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+
+    <title>Page Title</title>
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <form class="warehouseInfo">
+
+        <p class="warehouse-input-lbl">Material Name:</p>
+        <input class="warehouse-input" type="text">
+
+        <p class="warehouse-input-lbl">Code Name:</p>
+        <input class="warehouse-input" type="text">
+
+        <p class="warehouse-input-lbl">Supplier:</p>
+        <input class="warehouse-input" type="text">
+
+        <p class="warehouse-input-lbl">Date Released:</p>
+        <input class="warehouse-input" type="date">
+
+        <p class="warehouse-input-lbl">Released Volume:</p>
+        <input class="warehouse-input" type="text">
+
+        <br>
+
+        <input class="warehouse-btn" type="button" value="Submit">
+
+    </form>
+
+    <script src="../javascript/web.js"></script>
+
+</body>
+
+</html>
