e7e6b4b5e73c58903d77b470c36059c6f0cf6d2c
raven berenguila <you@example.com>
2023-09-15 15:57:21 +0800

    .

--- thesis1_website/loginpage/index.php
+++ thesis1_website/loginpage/index.php
@@ -1,13 +1,3 @@
-<?php
-session_start();
-            if (isset($_SESSION['username'])) {
-                // Redirect the user back to the previous page
-                header("Location: ../Homepage");
-                exit();
-            }
-?>
-
-
 <!DOCTYPE html>
 
 <html>
@@ -20,74 +10,29 @@ session_start();
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
-    <link rel="stylesheet" href="logInPage.css">
-
+    <link rel="stylesheet" href="../css/style.css">
 </head>
 
 <body>
 
-    <div class="log">
-        <?php
-        include('../includesPHP/topNav.php');
+    <div class="logInPage">
 
-        
-        ?>
+        <img src="../image\logo/KBN_Logo.png" class="logInPage-logo">
 
-        
-    </div>
-
-    <div class="logInCon">
-
-        <form method="post" action="<?php echo $_SERVER['PHP_SELF'];?>">
-
-            <p class="LoginTitle">Log In</p>
-                <?php
-                    if ($_SERVER["REQUEST_METHOD"] == "POST") {
-                        $uName = $_POST['txtUsername'];
-                        $pWord = $_POST['txtPassword'];
-                        if (empty($uName) || empty($pWord)) {
-                            echo "Please fill the username/password";
-                        } else if($uName == "admin" && $pWord == "admin"){
-                                if (session_status() === PHP_SESSION_ACTIVE) {
-                                    $_SESSION['username'] = $uName;
-                                    $_SESSION['password'] = $pWord;
-                                    header("Location: ../Homepage");
-                                    exit();
-                                } else {
-                                    echo "not started";
-                                }
-                        }else{
-                            echo "Incorrect username/password";
-                        }
-                    }
-                ?><br>
-            <label class="loginLabel">User Name</label>
-            <input placeholder="Username" name="txtUsername"><br><br><br>
-    
-            <label class="loginLabel">Password</label>
-            <input placeholder="Enter your Password" name="txtPassword"><br><br>  
+        <div class="inner-logInPage">
             
-            <br>
-        
-            <button>Log In</button>
-            
-        </form>
-        
-        
+            <input class="logInPage-input" placeholder="Username" type="text">
+
+            <input class="logInPage-input" placeholder="Password" type="text">
+
+            <input class="warehouse-btn" type="button" value="Log In">
+
+        </div>
+
     </div>
-
-
-
-
-    <br><br>
-
-    
-
-    <?php include('../includesPHP/footer.php')?>
 
     <script src="../javascript/web.js"></script>
 
 </body>
 
 </html>
-
