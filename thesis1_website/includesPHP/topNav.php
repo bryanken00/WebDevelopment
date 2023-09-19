0a8c1fd9e21084735c1b028187f4cc8a9f6cc024
raven berenguila <you@example.com>
2023-09-20 01:12:06 +0800

    .

--- thesis1_website/includesPHP/topNav.php
+++ thesis1_website/includesPHP/topNav.php
@@ -47,6 +47,11 @@ if(session_status() == PHP_SESSION_NONE)
 
             </div>
 
+            <div class="searchBar">
+                <svg class="searchIcon" aria-hidden="true" viewBox="0 0 24 24"><g><path d="M21.53 20.47l-3.66-3.66C19.195 15.24 20 13.214 20 11c0-4.97-4.03-9-9-9s-9 4.03-9 9 4.03 9 9 9c2.215 0 4.24-.804 5.808-2.13l3.66 3.66c.147.146.34.22.53.22s.385-.073.53-.22c.295-.293.295-.767.002-1.06zM3.5 11c0-4.135 3.365-7.5 7.5-7.5s7.5 3.365 7.5 7.5-3.365 7.5-7.5 7.5-7.5-3.365-7.5-7.5z"></path></g></svg>
+                <input placeholder="Search" type="search" class="searchInput">
+            </div>
+
             <input type="checkbox" id="check-compressed-btn">
         
                 <div class="mobile-compressed">
@@ -122,10 +127,6 @@ if(session_status() == PHP_SESSION_NONE)
                 <!-- <a class="icn" id="cartBtn" onclick="logInBtnFunc()">
                     <i class="fa-sharp fa-solid fa-user"></i>
                 </a> -->
-
-                <a class="icn" href="#">
-                    <i class="fa-solid fa-magnifying-glass"></i>
-                </a>
 
             </div>            
 
