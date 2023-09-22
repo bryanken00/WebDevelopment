226a3421fbcf1ca10126dea9a855411dccbcf3ae
bryanken00 <bryanken01230@gmail.com>
2023-09-23 00:44:07 +0800

    upload php

--- thesis1_website/Products/upload.php
+++ thesis1_website/Products/upload.php
@@ -3,8 +3,9 @@ $targetDirectory = "resources/"; // The directory where you want to save the upl
 $targetFile = $targetDirectory . basename($_FILES["file"]["name"]);
 
 if (move_uploaded_file($_FILES["file"]["tmp_name"], $targetFile)) {
-    echo "File uploaded successfully.";
+    $uploadedFileName = basename($_FILES["file"]["name"]); // Get the file name only
+    echo $uploadedFileName;
 } else {
-    echo "Error uploading file.";
+    echo json_encode(array("message" => "Error uploading file."));
 }
 ?>
