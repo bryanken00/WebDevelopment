c30a4c071199d64a9e409d17d52d1e1e9d4c427c
bryanken00 <bryanken01230@gmail.com>
2023-10-14 21:34:32 +0800

    test

--- thesis1_website/includesPHP/database.php
+++ thesis1_website/includesPHP/database.php
@@ -1,16 +1,16 @@
 <?php
 
-$servername = "localhost";
-$username   = "root";
-$password   = "";
-$dbname     = "kbndatabase";
+$servername = "srv1091.hstgr.io:3306";
+$username = "u381024298_kbndatabase";
+$password = "2wQG;a?N";
+$database = "u381024298_kbndatabase";
 
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $database);
 
 if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "Database failed to connect";
+    die("Connection failed: " . $mysqli->connect_error);
 }
+
 
 
 // function getUserID($userName){
