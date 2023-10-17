def1ae6b9d2d72854eec707d8de87dc4524c11da
bryanken00 <bryanken01230@gmail.com>
2023-10-17 13:09:11 +0800

    home page

--- thesis1_website/index.php
+++ thesis1_website/index.php
@@ -1,21 +1,5 @@
 <!-- Session Destroyer -->
 
 <?php
-session_start();
-session_destroy();
-
-echo "Username: " . $_SESSION['username'] . "\nPassword: " . $_SESSION['password'] . "\nUserID: " . $_SESSION['userID'];
-
-include('includesPHP/database.php');
-
-
-$result = $conn->query("SELECT count(*) FROM tblorders");
-
-if (!$result) {
-    echo "Error executing query: " . $conn->error;
-} else {
-
-    $row = $result->fetch_row();
-    echo "Count of orders: " . $row[0];
-}
+echo "<script>window.location.href = 'homepage/';</script>";
 ?>
