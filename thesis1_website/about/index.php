6ef21e6a2027a9e9b7286dfb844ea7ce5b87149d
raven berenguila <you@example.com>
2023-10-18 23:28:36 +0800

    .

--- thesis1_website/about/index.php
+++ thesis1_website/about/index.php
@@ -7,7 +7,7 @@ if(session_status() == PHP_SESSION_NONE)
 <html>
 <head>
 
-<title>Page Title</title>
+<title>About</title>
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8">
