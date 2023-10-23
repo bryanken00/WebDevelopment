8442f19b6b9cc3e567d22823c96cfe2102f8acf2
bryanken00 <bryanken01230@gmail.com>
2023-10-23 08:55:13 +0800

    final test

--- thesis1_website/homepage/index.php
+++ thesis1_website/homepage/index.php
@@ -11,7 +11,7 @@ unset($_SESSION['checkedCheckboxesData']);
 
 <head>
 
-    <title>test</title>
+    <title>KissedByNature</title>
     <link rel="icon" href="../icon/icon.png" type="image/x-icon">
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
