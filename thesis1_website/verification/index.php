d4634a3a5bb2bd6280a39537492c9c5e4000ddd6
raven berenguila <you@example.com>
2023-10-23 11:53:12 +0800

    .

---
+++ thesis1_website/verification/index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <title>Email Verification</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+
+    <link rel="stylesheet" href="../css/style.css">
+</head>
+
+<body>
+
+    <form class="form">
+        <span class="close-form">X</span>
+
+        <div class="info">
+            <span class="title">E-Mail Verification</span>
+            <p class="description">You must enter the verification code we sent to your email.</p>
+        </div>
+        <div class="input-fields">
+            <input placeholder="" type="tel" maxlength="1">
+            <input placeholder="" type="tel" maxlength="1">
+            <input placeholder="" type="tel" maxlength="1">
+            <input placeholder="" type="tel" maxlength="1">
+        </div>
+
+        <div class="action-btns">
+            <a class="verify" href="#">Verify</a>
+            <a class="clear" href="#">Clear</a>
+        </div>
+
+    </form>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script src="../javascript/returnProduct.js"></script>
+    
+</body>
+</html>
