ce7a2d2f520195576e7565cf4fb4857f8983a984
bryanken00 <bryanken01230@gmail.com>
2023-10-26 22:44:14 +0800

    MySQL Fix

--- thesis1_website/includesPHP/database.php
+++ thesis1_website/includesPHP/database.php
@@ -1,38 +1,17 @@
 <?php
+    try{
+        $servername = 'admin.cjwov25fsy4k.ap-southeast-2.rds.amazonaws.com:3306'; // Replace with your MySQL host
+        $username = 'admin'; // Replace with your MySQL username
+        $password = 'KENKENken0011'; // Replace with your MySQL password
+        $dbname = 'kbndatabase'; // Replace with your MySQL database name
 
-// $servername = "localhost";
-// $username   = "root";
-// $password   = "";
-// $dbname     = "kbndatabase";
+        $conn = new mysqli($servername, $username, $password, $dbname);
 
-$servername = 'kbnthesis.c2ezvppz4be9.ap-southeast-1.rds.amazonaws.com:3306'; // Replace with your MySQL host
-$username = 'admin'; // Replace with your MySQL username
-$password = 'KENKENken0011'; // Replace with your MySQL password
-$dbname = 'kbndatabase'; // Replace with your MySQL database name
-
-$conn = new mysqli($servername, $username, $password, $dbname);
-
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-    echo "Database failed to connect";
-}
-
-
-
-// function getUserID($userName){
-//     $sql = "SELECT UserID FROM tblOrders WHERE userName = '$data'";
-//     $result = $conn->query($sql);
-//     $row = $result->fetch_assoc();
-//     $data = $row['OrderList'];
-//     return $data;
-// }
-
-
-// function getUserInfo($userID){
-//     $sql = "SELECT * FROM tblcustomerinformation WHERE userID = '$userID'";
-//     $result = $conn->query($sql);
-//     $row = $result->fetch_assoc();
-//     $array = array($row['Firstname'], $row['Lastname'], $row['Number']);
-//     return $array;
-// }
+        if ($conn->connect_error) {
+            die("Connection failed: " . $conn->connect_error);
+            echo "Database failed to connect";
+        }
+    }catch(Exception $e){
+        echo "<script>window.location.href = '../server.php';</script>";
+    }
 ?>
--- thesis1_website/server.php
+++ thesis1_website/server.php
@@ -1,5 +1,6 @@
 <html>
     <body>
         <h1>SERVER MAINTENANCE</h1>
+        <a href="homepage/"><h2>Reconnect</h2></a>
     </body>
 </html>
