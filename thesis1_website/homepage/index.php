a7e4b3d36de09ff99b4fecc2713272e1e30215a5
raven berenguila <you@example.com>
2023-11-01 18:56:17 +0800

    .

--- thesis1_website/homepage/index.php
+++ thesis1_website/homepage/index.php
@@ -13,6 +13,7 @@ unset($_SESSION['emailAddress']);
 <head>
 
     <title>KissedByNature</title>
+
     <link rel="icon" href="../icon/icon.png" type="image/x-icon">
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -20,6 +21,8 @@ unset($_SESSION['emailAddress']);
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
     <link rel="stylesheet" href="../css/style.css">
+
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Trirong">
 
 </head>
 
