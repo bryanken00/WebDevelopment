5dbfe762ab9f91e3c89e729b1a11395d2084bce3
bryanken00 <bryanken01230@gmail.com>
2023-11-05 01:46:48 +0800

    test

--- thesis1_website/verification/index.php
+++ thesis1_website/verification/index.php
@@ -4,16 +4,6 @@
 
 include('../includesPHP/database.php');
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
-
-require '../PHPMailer/src/PHPMailer.php';
-require '../PHPMailer/src/SMTP.php';
-require '../PHPMailer/src/Exception.php';
-
-$mail = new PHPMailer(true);
-
 function generateVerificationCode() {
     $characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'; // You can use other characters as well
     $code = '';
@@ -97,6 +87,16 @@ $html = "<!DOCTYPE html>
 </html>
 ";
 
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
+
+require '../PHPMailer/src/PHPMailer.php';
+require '../PHPMailer/src/SMTP.php';
+require '../PHPMailer/src/Exception.php';
+
+$mail = new PHPMailer(true);
+
 try {
     // Server settings
     $mail->SMTPDebug = SMTP::DEBUG_OFF; // Disable verbose debug output
@@ -109,20 +109,21 @@ try {
     $mail->Port = 465; // Use the SSL port (465 for SMTPS)
 
     // Recipients
-    $mail->setFrom('verify@kissedbynature.online', 'Kissed By Nature');
-    $mail->addAddress($emailAddress);
+    $mail->setFrom('verify@kissedbynature.online', 'Your Name');
+    $mail->addAddress('bryanken01230@gmail.com', 'Recipient Name');
 
     // Content
     $mail->isHTML(true);
-    $mail->Subject = 'Verification Code';
-    $mail->Body = $html;
+    $mail->Subject = 'Your Subject Here';
+    $mail->Body = 'Your message here.';
 
     // Avoiding spam complaints
     $mail->AddCustomHeader('List-Unsubscribe: <mailto:unsubscribe@example.com>');
 
     $mail->send();
+    echo 'Message has been sent';
 } catch (Exception $e) {
-    echo 'Email sending failed: ' . $mail->ErrorInfo;
+    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
 
 ?>
