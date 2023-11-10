d458806263d78daaeddac7d0a8678775d09da3d2
raven berenguila <you@example.com>
2023-11-11 01:54:55 +0800

    .

--- thesis1_website/homepage/index.php
+++ thesis1_website/homepage/index.php
@@ -8,6 +8,8 @@ unset($_SESSION['emailAddress']);
 
 <!DOCTYPE html>
 
+<html lang="en">
+
 <html>
 
 <head>
@@ -17,6 +19,8 @@ unset($_SESSION['emailAddress']);
     <link rel="icon" href="../icon/icon.png" type="image/x-icon">
     
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <meta charset="UTF-8">
 
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css" integrity="sha512-SzlrxWUlpfuzQ+pcUCosxcglQRNAq/DZjVsC0lE40xsADsfeQoEypE+enwcOiGjk/bSuGGKHEyjSoQ1zVisanQ==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 
